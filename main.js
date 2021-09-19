488b6971b46737a8e5cd74e66a11b1174731ae5d
jlopcun <62543437+jlopcun@users.noreply.github.com>
2021-09-19 10:21:13 +0200

    push

---
+++ main.js
@@ -0,0 +1,11 @@
+const clock = document.getElementById('clock');
+
+setInterval(()=>{
+    clock.textContent = new Date().toLocaleTimeString();
+    if(hour<=12) clock.textContent +=` AM`;
+    else clock.textContent +=` PM`;
+    
+},1000);
+
+const hour = new Date().getHours();
+console.log(hour)
